055a484aadf063fadd99486cb2011295588dab7d
payal235714 <paldabasis79@gmail.com>
2025-05-23 21:54:10 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "ℳ𝒟 𝒫𝒜𝒴𝒜ℒ",
-  botOwner: '𝒫𝒜𝒴𝒜ℒ', // Your name
+  botOwner: '𝒫𝒜𝒴𝒜ℒ.', // Your name
   ownerNumber: '919800510991', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
